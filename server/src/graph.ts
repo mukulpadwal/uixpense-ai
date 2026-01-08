bdf31c370bcebae983198ffa7f151bd05306ff3c
mukulpadwal <mukulpadwal.me@gmail.com>
2026-01-08 21:55:01 +0530

    feat: give database access to tools.

--- server/src/graph.ts
+++ server/src/graph.ts
@@ -1,8 +1,30 @@
 import { MessagesAnnotation } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 import initDB from "./db.ts";
+import initTools from "./tools.ts";
+import llm from "./llm.ts";
 
 const database = initDB("./expenses.db");
+const tools = initTools(database);
 
-async function callModel(state: typeof MessagesAnnotation.State) {
-  return state;
-}
+const toolNode = new ToolNode(tools);
+
+const callModel = async (state: typeof MessagesAnnotation.State) => {
+  const llmWithTools = llm.bindTools(tools);
+
+  const response = await llmWithTools.invoke([
+    {
+      role: "system",
+      content: `
+        You are a helpful assistant that can help the user manage their expenses.
+        Current DateTime information: ${new Date().toISOString()}
+
+        The user can use the following tools:
+        ${tools.map((tool) => tool.name).join(", ")}
+      `,
+    },
+    ...state.messages,
+  ]);
+
+  return { messages: [response] };
+};
--- server/src/tools.ts
+++ server/src/tools.ts
@@ -1,29 +1,35 @@
+import { DatabaseSync } from "node:sqlite";
 import * as z from "zod";
 import { tool } from "langchain";
 
-const addExpense = tool(
-  ({ title, amount, category, description }) => {
-    
-    console.log({
-      title,
-      amount,
-      category,
-      description,
-    });
+const initTools = (database: DatabaseSync) => {
+  const addExpense = tool(
+    ({ title, amount, category, description }) => {
+      console.log({
+        title,
+        amount,
+        category,
+        description,
+      });
 
-    return JSON.stringify({
-      success: true,
-      message: "Expense added successfully",
-    });
-  },
-  {
-    name: "add_expense",
-    description: "Tool to add an expense to the database.",
-    schema: z.object({
-      title: z.string().describe("Title of the expense"),
-      amount: z.number().describe("Amount of the expense"),
-      category: z.string().describe("Category of the expense"),
-      description: z.string().describe("Description of the expense"),
-    }),
-  },
-);
+      return JSON.stringify({
+        success: true,
+        message: "Expense added successfully",
+      });
+    },
+    {
+      name: "add_expense",
+      description: "Tool to add an expense to the database.",
+      schema: z.object({
+        title: z.string().describe("Title of the expense"),
+        amount: z.number().describe("Amount of the expense"),
+        category: z.string().describe("Category of the expense"),
+        description: z.string().describe("Description of the expense"),
+      }),
+    },
+  );
+
+  return [addExpense];
+};
+
+export default initTools;
