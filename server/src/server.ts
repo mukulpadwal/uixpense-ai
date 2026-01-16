8d9f8d427bed9ea45f86db9ab860ce97f8e8b27e
mukulpadwal <mukulpadwal.me@gmail.com>
2026-01-16 20:41:41 +0530

    feat: convert chat endpoint to handle post request

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
@@ -7,16 +8,18 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/chat", (req: Request, res: Response) => {
+app.post("/chat", (req: Request, res: Response) => {
   // 1. Set Special Headers
   res.writeHead(200, {
     "content-type": "text/event-stream",
   });
 
   // 2. Send data in special format
-  res.write("event: ping\n");
-  res.write("data: Pong");
-  res.write("\n\n");
+  setInterval(() => {
+    res.write("event: ping\n");
+    res.write("data: Pong");
+    res.write("\n\n");
+  }, 1000);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
