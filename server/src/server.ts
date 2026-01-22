cb816cd2c88d4d368b33a6f820c88576a3536fd4
mukulpadwal <mukulpadwal.me@gmail.com>
2026-01-22 17:02:40 +0530

    fix: cors error

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,13 @@ const limiter = rateLimit({
 });
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  }
+));
 app.use(express.json());
 app.use(limiter);
 
