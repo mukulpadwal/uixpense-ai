ee43576ce832ce6fe8ce1e4c8641e249f1303b88
mukulpadwal <mukulpadwal.me@gmail.com>
2026-01-27 17:36:16 +0530

    feat: add initial backend health check on mount.

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 
 function RootLayout() {
+  useEffect(() => {
+    async function healthCheck() {
+      await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/health`);
+    }
+
+    healthCheck();
+  }, [])
+
   return <Outlet />;
 }
 
